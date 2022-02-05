3dca5166ce94d43ef821a8139d79c3115f7e2c91
Mark Lister <mjwlister@icloud.com>
2022-02-05 11:46:03 +0000

    build

--- dist/vepple-embed.es.js
+++ dist/vepple-embed.es.js
@@ -293,7 +293,7 @@ function instance($$self, $$props, $$invalidate) {
 class Counter extends SvelteElement {
   constructor(options) {
     super();
-    this.shadowRoot.innerHTML = `<style>button{font-family:inherit;font-size:inherit;padding:1em 2em;color:#ff3e00;background-color:rgba(255, 62, 0, 0.1);border-radius:2em;border:2px solid rgba(255, 62, 0, 0);outline:none;width:200px;font-variant-numeric:tabular-nums;cursor:pointer}button:focus{border:2px solid #ff3e00}button:active{background-color:rgba(255, 62, 0, 0.2)}</style>`;
+    this.shadowRoot.innerHTML = `<style>button{font-family:inherit;font-size:inherit;padding:1em 2em;color:#ff3e00;background-color:var(--color, red);border-radius:2em;border:2px solid rgba(255, 62, 0, 0);outline:none;width:200px;font-variant-numeric:tabular-nums;cursor:pointer}button:focus{border:2px solid #ff3e00}button:active{background-color:rgba(255, 62, 0, 0.2)}</style>`;
     init(this, {
       target: this.shadowRoot,
       props: attribute_to_object(this.attributes),
